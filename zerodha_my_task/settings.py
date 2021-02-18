e8a80f94546f73b99c55b28fd7cefe5c519eba97
Surya Peruri <suryasrikar1997@gmail.com>
2021-02-18 20:39:24 +0530

    fix:gcp ip address added

--- zerodha_my_task/settings.py
+++ zerodha_my_task/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'g)6rsx*yxx%1z2^(rm3xzwr42k_n5_lby%5hctzbwy1!o-y2-j'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0', '9ae66c3bf01b.ngrok.io']
+ALLOWED_HOSTS = ['localhost', '127.0.0.1', '0.0.0.0']
 
 
 # Application definition
